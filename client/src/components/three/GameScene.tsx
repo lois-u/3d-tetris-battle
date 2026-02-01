9195cfe665d6aa8465a035f456215bbe2c018b81
lois <lois@lguplus.co.kr>
2026-02-01 14:48:35 +0900

    fix: reduce opponent name size and position below board

--- client/src/components/three/GameScene.tsx
+++ client/src/components/three/GameScene.tsx
@@ -64,12 +64,12 @@ function OpponentBoard({ opponent, position }: OpponentBoardProps) {
         <meshBasicMaterial color={opponent.isAlive ? '#1a1a2e' : '#3a1a1a'} transparent opacity={0.8} />
       </mesh>
       <Text
-        position={[5, -2, 0]}
-        fontSize={1.5}
+        position={[5, -3, 1]}
+        fontSize={0.9}
         color={opponent.isAlive ? '#ffffff' : '#ff4444'}
         anchorX="center"
-        anchorY="middle"
-        outlineWidth={0.05}
+        anchorY="top"
+        outlineWidth={0.03}
         outlineColor="#000000"
       >
         {opponent.name}
